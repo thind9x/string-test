c849d2dc31d3b3ba3fa98792ee93c4bef719275d
thind9x <thind9x@gmail.com>
2022-07-09 09:56:51 +0700

    fix(stringee):  new tokten

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect,useMemo} from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 // @ts-ignore
-import { StringeeClient, StringeeChat } from "stringee-chat-js-sdk";
+import {StringeeChat, StringeeClient} from "stringee-chat-js-sdk";
+
 function App() {
 
   let stringeeClient: { on: (arg0: string, arg1: { (): void; (res: any): void; (res: any): void; }) => void; connect:(data:string)=>void };
@@ -10,7 +10,7 @@ function App() {
   // Init
   stringeeClient = new StringeeClient();
 
-  stringeeClient.connect('eyJjdHkiOiJzdHJpbmdlZS1hcGk7dj0xIiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJqdGkiOiJTS21YUG1kOEpaejJ1WUh6eWpTWTN5c3p3emFvMWFIN1VnLTE2NTcyNTM3MTkiLCJpc3MiOiJTS21YUG1kOEpaejJ1WUh6eWpTWTN5c3p3emFvMWFIN1VnIiwiZXhwIjoxNjU5ODQ1NzE5LCJ1c2VySWQiOiJQaGFtRGFuZ0tob2EifQ.bYzidvlKHsSOawM4lJdhIZN2tAxOqtVG3bEWWJax9fU');
+  stringeeClient.connect('eyJjdHkiOiJzdHJpbmdlZS1hcGk7dj0xIiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJqdGkiOiJTS1ZMUllRY0Q2Z3p2d1d4MnZlRUdoMU1xZzZHaXAwYVFtLTE2NTcyNzczNDMiLCJpc3MiOiJTS1ZMUllRY0Q2Z3p2d1d4MnZlRUdoMU1xZzZHaXAwYVFtIiwiZXhwIjoxNjU5ODY5MzQzLCJ1c2VySWQiOiJ0aGluZDl4In0.M2cjCCzhI2tiecHwfiqyTrpVKukqxsy-dZ-d3SW1P_s');
 
   // init
   var stringeeChat = new StringeeChat(stringeeClient);
@@ -21,25 +21,37 @@ function App() {
     isDistinct: false,
     isGroup: true
   };
-  var txtMsg = {
-    type: 1,
-    convId: "YOUR_CONVERSATION_ID",
-    message: {
-      content: 'Hello',
-      metadata: {
-        key: 'value'
-      }
-    }
-  };
+  const[msg,setMgs] = useState("");
 
   stringeeClient.on('authen', function () {
     // khởi tạo stringeeChat ở đây
-    stringeeChat.sendMessage(txtMsg, function (status:any, code:any, message:any, msg:any) {
-      console.log(status + code + message + "msg result " + JSON.stringify(msg));
-    });
+
     stringeeChat.createConversation(userIds, options, (status:any, code:any, message:any, conv:any) => {
-      console.log('status:' + status + ' code:' + code + ' message:' + message + ' conv:' + JSON.stringify(userIds));
+      var txtMsg = {
+        type: 1,
+        convId: conv?.lastMessage?.conversationId,
+        message: {
+          content: msg,
+          metadata: {
+            key: 'value'
+          }
+        }
+      };
+      stringeeChat.sendMessage(txtMsg, function (status:any, code:any, message:any, msg:any) {
+       console.log(msg);
+      });
     });
+
+    var convId = 'conv-vn-1-3QUQ1YWZ4G-1657040257363';
+    var count = 50;
+    var isAscending = false;
+    stringeeChat.getLastMessages(convId, count, isAscending, function (status:any, code:any, message:any, msgs:any) {
+      msgs?.map((it:any)=>{
+        console.log(it)
+      })
+    });
+
+
   })
 
 
@@ -49,20 +61,10 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id={"chat"}></p>
+      <textarea placeholder={"chat"} value={msg} onChange={(e)=>{
+        setMgs(e?.target?.value)
+      }}></textarea>
     </div>
   );
 }
